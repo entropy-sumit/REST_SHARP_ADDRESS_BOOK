1f706e05f25026befed7dc2a09867c1dd9b990e6
entropy-sumit <sumitr600@gmail.com>
2022-02-06 23:37:57 +0530

    [add] sumit added restsharp test of addressbook

---
+++ AddressBookRestSharp/UnitTest1.cs
@@ -0,0 +1,44 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace AddressBookRestSharp
+{
+    public class AddressBook
+    {
+        public int id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+
+    [TestClass]
+    public class UnitTest1
+    {
+        RestClient client;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            client = new RestClient(" http://localhost:3000");
+        }
+        private IRestResponse AddressbookList()
+        {
+            RestRequest request = new RestRequest("AddressBookJson", Method.GET);
+
+            IRestResponse response = client.Execute(request);
+            return response;
+        }
+       
+        
+    }
+}
+  
